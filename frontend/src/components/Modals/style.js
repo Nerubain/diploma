03f7e707a33085017cf7d8dd64722da587926fcf
nerubian <kiritogercy@gmail.com>
2020-04-10 01:35:03 +0300

    Add modal skeleton

---
+++ frontend/src/components/Modals/style.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const AddBoardButton = styled.button`
+  width: 100%;
+  text-align: left;
+  border: 0;
+  border-radius: 3px;
+  margin: 2px 4px 2px 2px;
+  padding: 6px 10px;
+  text-decoration: underline;
+  line-height: 20px;
+  font-weight: 400;
+  background-color: white;
+  cursor: pointer;
+
+  &:hover {
+    background-color: rgba(9, 30, 66, 0.08);
+  }
+`;
