f63ef05d4e68f07d4c59d28b5e821058045507af
nerubian <kiritogercy@gmail.com>
2020-05-05 19:11:37 +0300

    Add bcrypt module

---
+++ backend/src/modules/bcrypt.js
@@ -0,0 +1,11 @@
+import bcrypt from 'bcrypt';
+
+const saltRounds = 10;
+
+export const hashPassword = password => {
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hash = bcrypt.hashSync(password, salt);
+  return hash;
+};
+
+export const comparePasswords = (password, hashed) => bcrypt.compareSync(password, hashed);
